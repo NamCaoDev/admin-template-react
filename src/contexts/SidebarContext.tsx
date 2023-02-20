23e0afbdd41222e10935d00a8ea88e2a78fd2467
namcao2610 <namcaodev@gmail.com>
2023-02-20 10:38:27 +0700

    Init source

---
+++ src/contexts/SidebarContext.tsx
@@ -0,0 +1,33 @@
+import { FC, useState, createContext } from 'react';
+type SidebarContext = {
+  sidebarToggle: any;
+  toggleSidebar: () => void;
+  closeSidebar: () => void;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const SidebarContext = createContext<SidebarContext>(
+  {} as SidebarContext
+);
+
+interface SidebarProviderProps {
+  children: React.ReactNode;
+}
+
+export const SidebarProvider: FC<SidebarProviderProps> = ({ children }) => {
+  const [sidebarToggle, setSidebarToggle] = useState(false);
+  const toggleSidebar = () => {
+    setSidebarToggle(!sidebarToggle);
+  };
+  const closeSidebar = () => {
+    setSidebarToggle(false);
+  };
+
+  return (
+    <SidebarContext.Provider
+      value={{ sidebarToggle, toggleSidebar, closeSidebar }}
+    >
+      {children}
+    </SidebarContext.Provider>
+  );
+};
